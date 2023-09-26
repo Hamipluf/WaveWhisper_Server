1f9b105752909a7bc754679ee27e96d62ace41a9
Ramiro Gumma <ramirogumma@hotmail.com>
2023-09-26 11:17:08 -0300

    Fix bug Login

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -3,16 +3,27 @@ import {
   getAll,
   getOneById,
   register,
-  login
+  login,
 } from "../contollers/users.controller.js";
 import authenticateMiddleware from "../middlewares/auth.middleware.js";
 import isAdmin from "../middlewares/adminVerify.middleware.js";
 const router = Router();
-
-router.get("/", authenticateMiddleware, getAll);
+// Todos los users
+router.get(
+  "/",
+  //  authenticateMiddleware,
+  getAll
+);
 // Solamente authenticados y administradores tienen acceso a un solo user
-router.get("/:id", authenticateMiddleware, isAdmin, getOneById);
+router.get(
+  "/:id",
+  // authenticateMiddleware,
+  //  isAdmin,
+  getOneById
+);
+// Registrar user
 router.post("/register", register);
+// Logear user
 router.post("/login", login);
 
 export default router;
